1b93d4155af859680376b4f83e417eaab7ad2565
juliacouto026-dev <juliacouto026@gmail.com>
2026-05-04 22:29:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function alterarFundo(){
-document.body.style.backgroundColor="black";
-}
+const formulario=document.getElementById("meuFormulario");    <!-- Cria constante fomulario e pegua elemento por iD-->
+formulario.addEventListener("Submit",function(evento){    <!--Cria escutador de evento -> submit-->
+    evento.preventDefault();    <!--Nao recarregar pagina-->
+
+    <!-- Cria contantes e pegua elementos do formulario pelo Id, somente o valor-->
+    const novoNomea = document.GetElementById("aname").value;
+    const novoNomel= document.GetElementById("lname").value;
+    const novoImagem = document.GetElementById("imegem").value;
+
+    <!--Constante que grupa informações do formulario -->
+    const novo={
+    nomeAutor: novoNomea,
+    nomelivro: novoNomel,
+    imagem: novoImagem,
+    };
+})
+
+
+
