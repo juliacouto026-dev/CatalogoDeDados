9dac2dde2a7115f5858cd879059bc8384409e998
juliacouto026-dev <juliacouto026@gmail.com>
2026-05-11 11:30:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const formulario=document.getElementById("meuFormulario");    <!-- Cria constante fomulario e pegua elemento por iD-->
-formulario.addEventListener("Submit",function(evento){    <!--Cria escutador de evento -> submit-->
-    evento.preventDefault();    <!--Nao recarregar pagina-->
+const formulario=document.getElementById("meuFormulario");   // Cria constante fomulario e pegua elemento por iD//
+formulario.addEventListener("submit",function(evento){    //Cria escutador de evento -> submit//
+    evento.preventDefault();    //Nao recarregar pagina//
 
-    <!-- Cria contantes e pegua elementos do formulario pelo Id, somente o valor-->
-    const novoNomea = document.GetElementById("aname").value;
-    const novoNomel= document.GetElementById("lname").value;
-    const novoImagem = document.GetElementById("imegem").value;
+    //Cria contantes e pegua elementos do formulario pelo Id, somente o valor//
+    const novoNomea = document.getElementById("aname").value;
+    const novoNomel= document.getElementById("lname").value;
+    const novoImagem = document.getElementById("imagem").value;
 
-    <!--Constante que grupa informações do formulario -->
+    //Constante que grupa informações do formulario //
     const novo={
     nomeAutor: novoNomea,
-    nomelivro: novoNomel,
+    nomeLivro: novoNomel,
     imagem: novoImagem,
     };
+
+    //Criando card//
+    const card =`
+        <div class="card">
+            <img src="${novo.imagem}">
+            <h3>"${novo.nomeLivro}"</h3>
+            <h4>"${novo.nomeAutor}"</h4>
+           </div>
+    `;
+    const colecao = document.getElementById("colecao");
+    colecao.innerHTML += card;
+    formulario.reset();
+
+
 })
+
 
 
 
